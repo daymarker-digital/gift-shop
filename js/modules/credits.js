93225f53b7422a2b55f0d0443b96e14ab28d307f
Brenden Mersey <brenden.mersey@weareverypolite.com>
2023-05-18 17:08:34 -0700

    QC

--- js/modules/credits.js
+++ js/modules/credits.js
@@ -1,4 +1,4 @@
-const info = { company : 'Very Polite Agency', tagline : '"Always Be Polite™"',  version : '2.0' };
+const info = { company : 'The Gift Shop by Very Polite Agency', tagline : '"All the Best™"',  version : '2.0' };
 
 const init = () => {
   console.log( `${info.company} - ${info.tagline} - Version ${info.version}` );
